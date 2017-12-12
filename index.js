c7958548e1efec7d625a85841b14814408d7e3cd
molant <amolleda@gmail.com>
2017-12-12 10:34:25 -0800

    Example that breaks the new API with an SVG file

---
+++ index.js
@@ -0,0 +1,36 @@
+// Server configuration
+
+const html = `<img src="/image.svg" />`;
+const svg = `<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" width="400" height="180">
+<rect x="50" y="20" rx="20" ry="20" width="150" height="150" style="fill:red;stroke: black;stroke-width:5;opacity:0.5" />
+</svg>`;
+
+const express = require('express');
+const app = express();
+
+app.get('/', function (req, res) {
+    console.log('Server: HTML requested');
+    res.send(html);
+});
+
+app.get('/image.svg', function (req, res) {
+    console.log('Server: image requested');
+    res.setHeader('Content-Type', 'image/svg+xml');
+    res.send(svg);
+    res.end();
+});
+
+// jsdom API tests
+
+function newApi() {
+    const jsdom = require('jsdom');
+    const { JSDOM } = jsdom;
+    console.log('---------------');
+    console.log('Testing new API\n');
+
+    JSDOM.fromURL('http://localhost:3000', {
+        resources: 'usable'
+    });
+}
+
+app.listen(3000, newApi);
